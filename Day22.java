d570ed1aa2429029105639e6aa06c8a216e01c90
Dayananda  K S <dayanandaks045@gmail.com>
2026-02-22 22:36:57 +0530

    Add 2D array implementation in Day22.java
    Implement a 2D array and print its elements.

---
+++ Day22.java
@@ -0,0 +1,24 @@
+// This is the 2D Array...
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+OUTPUT :
+1 2 3 
+4 5 6 
+
+*/
