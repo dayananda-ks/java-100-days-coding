e5ac0b67ca50054baaf4323b3edca84332000a26
Dayananda  K S <dayanandaks045@gmail.com>
2026-04-07 22:21:53 +0530

    Update Day62.java

--- Day62.java
+++ Day62.java
@@ -3,17 +3,17 @@ class Solution {
         StringBuilder result = new StringBuilder();
         
         int index = a.length() - 1;
-        int j = b.length() - 1;
+        int anotherindex = b.length() - 1;
         int carry = 0;
-        while (index >= 0 || j >= 0 || carry == 1) {
+        while (index >= 0 || anotherindex >= 0 || carry == 1) {
             int sum = carry;
             if (index >= 0) {
                 sum += a.charAt(index) - '0';
                 index--;
             }
-            if (j >= 0) {
-                sum += b.charAt(j) - '0';
-                j--;
+            if (anotherindex >= 0) {
+                sum += b.charAt(anotherindex) - '0';
+                anotherindex--;
             }
 
             result.append(sum % 2);
